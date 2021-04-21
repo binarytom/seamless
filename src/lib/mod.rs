ba0f43f752f64f287fdf1b14bf633a3fa31d013d
lieh <lienhe@xiaomi.com>
2021-04-21 20:29:18 +0800

    update platform window

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,3 +1,6 @@
+#![feature(min_type_alias_impl_trait)]
+#![feature(impl_trait_in_bindings)]
+pub mod platform;
 pub mod rpc;
 //pub mod platform;
 //pub mod net;
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
+#![feature(min_type_alias_impl_trait)]
+#![feature(impl_trait_in_bindings)]
 mod lib;
 use async_std::io;
 use async_std::task;
 use io::Result;
-use lib::rpc::rpc_client::RpcClient;
-use lib::rpc::rpc_client_proxy::RpcClientProxy;
-use lib::rpc::rpc_message::RpcMessage;
-use lib::rpc::rpc_server::RpcServer;
 use std::sync::mpsc;
 use std::{thread, time};
 
 fn main() -> Result<()> {
+    test_window();
+    Ok(())
+}
+
+fn test_window() {
+    use lib::platform::window::Window;
+    use lib::platform::x_windows_monitor::XWindows;
+
+    let x_win = XWindows::new();
+    x_win.x_hide_cursor();
+}
+
+fn test_rpc() {
+    use lib::rpc::rpc_client::RpcClient;
+    use lib::rpc::rpc_client_proxy::RpcClientProxy;
+    use lib::rpc::rpc_message::RpcMessage;
+    use lib::rpc::rpc_server::RpcServer;
     // task::block_on(RpcServer::accept_loop())
     // let content = "Hello World!".to_string();
     // let rpc_message = RpcMessage::ContentMessage { content };
